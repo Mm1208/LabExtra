327be81fbd1fec496f5382e5fe8ed647fa68e8ab
jo13bc <jo13bc@gmail.com>
2020-03-06 10:58:20 -0600

    Change second activity

--- LabExtraI/app/src/main/java/com/example/labextrai/SecondActivity.java
+++ LabExtraI/app/src/main/java/com/example/labextrai/SecondActivity.java
@@ -1,19 +1,21 @@
 package com.example.labextrai;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_second);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
