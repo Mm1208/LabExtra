38d864fc4aae4d54ebe38ad602decd24a444aa1e
Mm1208 <41961128+Mm1208@users.noreply.github.com>
2020-03-06 11:01:06 -0600

    main

--- LabExtraI/app/src/main/java/com/example/labextrai/MainActivity.java
+++ LabExtraI/app/src/main/java/com/example/labextrai/MainActivity.java
@@ -3,17 +3,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.widget.Toolbar;
+
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
@@ -95,7 +92,6 @@ public class MainActivity extends AppCompatActivity
         // The activity is about to be destroyed.
     }
 
-    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
@@ -117,7 +113,7 @@ public class MainActivity extends AppCompatActivity
 
     public void abrirMAct() {
         finish();
-        Intent a = new Intent(this, MainActivity2.class);
+        Intent a = new Intent(this, SecondActivity.class);
         startActivity(a);
     }
 
